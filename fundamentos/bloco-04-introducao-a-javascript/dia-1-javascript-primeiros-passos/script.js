78a0830d32961ab2eccb758db33d208950d5b644
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-09-21 19:53:45 -0300

    Exercício 3: define condição que testa se o numero 3 é o maior

--- fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/script.js
@@ -54,4 +54,6 @@ if (num1 > num2 && num1 > num3) {
     console.log('num1 é o maior dentre os 3 números')
 } else if (num2 > num1 && num2 > num3) {
     console.log('num2 é o maior dentre os 3 números')
+} else if (num3 > num1 && num3 > num2) {
+    console.log('num3 é o maior dentre os 3 números')
 }
