ea71c5c43ac51463d0c71f2c6cc6ee08e211771e
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-09-21 19:48:50 -0300

    Exercício 3: define condição que testa se o número 1 é o maior

--- fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/script.js
@@ -1,45 +1,55 @@
 //Exercício 1
 
-const a = 5; 
-const b = 10; 
+// const a = 5; 
+// const b = 10; 
 
-let operacao = '';
-let resultado;
+// let operacao = '';
+// let resultado;
 
-switch (operacao) {
-    case 'Adição': 
-        resultado = a + b;
-        console.log(resultado)
-        break;
-    case 'Subtração': 
-        resultado = a - b;
-        console.log(resultado)
-        break;
-    case 'Multiplicação':
-        resultado = a * b;
-        console.log(resultado)
-        break;
-    case 'Divisão':
-        resultado = a / b;
-        console.log(resultado)
-        break;
-    case 'Módulo': 
-        resultado = a % b;
-        console.log(resultado)
-        break;
-    default:
-        console.log('Insira operação válida')
-}
+// switch (operacao) {
+//     case 'Adição': 
+//         resultado = a + b;
+//         console.log(resultado)
+//         break;
+//     case 'Subtração': 
+//         resultado = a - b;
+//         console.log(resultado)
+//         break;
+//     case 'Multiplicação':
+//         resultado = a * b;
+//         console.log(resultado)
+//         break;
+//     case 'Divisão':
+//         resultado = a / b;
+//         console.log(resultado)
+//         break;
+//     case 'Módulo': 
+//         resultado = a % b;
+//         console.log(resultado)
+//         break;
+//     default:
+//         console.log('Insira operação válida')
+// }
 
 // Exercício 2
 
-const num1 = 10; 
-const num2 = 5; 
+// const num1 = 10; 
+// const num2 = 5; 
 
-if (num1 > num2) {
-    console.log('num1 é maior que num2')
-} else if (num2 > num1) {
-    console.log('num2 é maior que num1')
-} else {
-    console.log('Os números são iguais')
+// if (num1 > num2) {
+//     console.log('num1 é maior que num2')
+// } else if (num2 > num1) {
+//     console.log('num2 é maior que num1')
+// } else {
+//     console.log('Os números são iguais')
+// }
+
+// Exercício 3 
+
+const num1 = 5;
+const num2 = 10; 
+const num3 = 15; 
+
+if (num1 < num2 && num1 <num3) {
+    console.log('num1 é o maior dentre os 3 números')
 }
