f5fa80afa9984c578d9ba5db02e6e645280c92f6
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-09-21 19:40:22 -0300

    Exercício 2: cria estrutura condicional que retorna qual número é maior

--- fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/script.js
@@ -35,3 +35,11 @@ switch (operacao) {
 
 const num1 = 10; 
 const num2 = 5; 
+
+if (num1 > num2) {
+    console.log('num1 é maior que num2')
+} else if (num2 > num1) {
+    console.log('num2 é maior que num1')
+} else {
+    console.log('Os números são iguais')
+}
