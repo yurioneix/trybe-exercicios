210bf4d524bd291f202189a67701f6fc0ab651c6
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-09-21 19:24:18 -0300

    Cria condicional para operação de adição

--- fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/script.js
@@ -2,3 +2,13 @@
 
 const a = 5; 
 const b = 10; 
+
+let operacao = 'Adição';
+let resultado;
+
+switch (operacao) {
+    case 'Adição': 
+    resultado = a + b;
+    console.log(resultado)
+    break;
+}
