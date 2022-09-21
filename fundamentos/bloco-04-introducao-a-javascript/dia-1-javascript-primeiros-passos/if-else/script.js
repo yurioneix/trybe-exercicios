23b86cf58868aaa29b625fdfdf77c022c6a2fe2b
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-09-21 18:23:24 -0300

    Cria estrutura condicional para saber se o aluno foi aprovado ou não

--- fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/if-else/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/if-else/script.js
@@ -1,1 +1,9 @@
-const notaPessoaCandidata = 70; 
+const notaPessoaCandidata = 90; 
+
+if (notaPessoaCandidata >= 80) {
+    console.log('Parabéns, você foi aprovada(o)')
+} else if (notaPessoaCandidata < 80 && notaPessoaCandidata >= 60) {
+    console.log('Você está na nossa lista de espera')
+} else {
+    console.log('Você foi reprovada(o)')
+}
