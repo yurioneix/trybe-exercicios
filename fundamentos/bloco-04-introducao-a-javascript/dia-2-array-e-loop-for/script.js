a95a2623b36376b4e8be5a9420cb7cf2097cbe4a
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-09-25 17:20:12 -0300

    Exercício 2: soma todos os valores do array

--- fundamentos/bloco-04-introducao-a-javascript/dia-2-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-2-array-e-loop-for/script.js
@@ -1,5 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index = 0; index < numbers.length; index +=1) {
-    console.log(numbers[index]);
+//Exercício 1
+// for (let index = 0; index < numbers.length; index +=1) {
+//     console.log(numbers[index]);
+// }
+
+//Exercício 2
+let sum = 0; 
+
+for (let index = 0; index < numbers.length; index += 1) {
+    sum += numbers[index];
 }
+
+console.log(sum)
