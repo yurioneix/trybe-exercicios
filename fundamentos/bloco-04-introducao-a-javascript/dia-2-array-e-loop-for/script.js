a210f5a733cdc726c10a9e885d38b362451a879a
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-09-25 17:38:16 -0300

    Exercício 5: descobre qual o maior valor do array

--- fundamentos/bloco-04-introducao-a-javascript/dia-2-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-2-array-e-loop-for/script.js
@@ -14,16 +14,27 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // console.log(sum)
 
 //Exercício 3
-let average = 0; 
+// let average = 0; 
 
-for(let index = 0; index < numbers.length; index += 1) {
-    average += numbers[index];
-}
-console.log(average / numbers.length)
+// for(let index = 0; index < numbers.length; index += 1) {
+//     average += numbers[index];
+// }
+// console.log(average / numbers.length)
 
 // Exercício 4
-if (average > 20) {
-    console.log('Valor maior que 20')
-} else if (average < 20) {
-    console.log('Valor menor ou igual a 20')
+// if (average > 20) {
+//     console.log('Valor maior que 20')
+// } else if (average < 20) {
+//     console.log('Valor menor ou igual a 20')
+// }
+
+// Exercício 5 
+
+let bigger = 0; 
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (bigger < numbers[index]) {
+        bigger = numbers[index];
+    }
 }
+console.log(bigger)
