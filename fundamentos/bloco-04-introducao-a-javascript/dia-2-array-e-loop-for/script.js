f997a0a24439d49766ad9e96545a54448c12a9b5
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-09-25 19:31:41 -0300

    Exercício 9: utilizando o array anterior imprime a divisão de cada numero por 2

--- fundamentos/bloco-04-introducao-a-javascript/dia-2-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-2-array-e-loop-for/script.js
@@ -71,4 +71,10 @@ let array = [];
 for (let index = 1; index <= 25; index += 1) {
     array.push(index)
 }
-console.log(array)
+// console.log(array)
+
+// Exercício 9 
+
+for (let index = 0; index < array.length; index += 1) {
+    console.log(array[index] / 2)
+}
