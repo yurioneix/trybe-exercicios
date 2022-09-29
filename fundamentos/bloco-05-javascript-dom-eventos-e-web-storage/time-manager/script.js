a590b23aa385c03ce4589541753865768c8ad5bd
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-09-28 23:16:17 -0300

    Captura busca do body e define estilo

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/time-manager/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/time-manager/script.js
@@ -1,3 +1,5 @@
 const header = document.getElementById('header-container');
+const body = document.querySelector('body')
 
 header.style.backgroundColor = 'green';
+body.style.backgroundColor = 'lightgrey'
