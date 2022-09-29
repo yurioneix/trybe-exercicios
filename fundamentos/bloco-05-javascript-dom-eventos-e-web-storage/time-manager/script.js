e248f4d3acfd700c6e214cc1cf1b89f0bf2fc38b
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-09-28 23:31:12 -0300

    Armazena busca das divs urgentes e cria loop para definir cor

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/time-manager/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/time-manager/script.js
@@ -1,5 +1,10 @@
 const header = document.getElementById('header-container');
-const body = document.querySelector('body')
+const body = document.querySelector('body');
+const urgentDivs = document.getElementsByClassName('emergency-tasks');
 
 header.style.backgroundColor = 'rgb(0,176,105)';
-body.style.backgroundColor = 'rgb(243,243,243)'
+body.style.backgroundColor = 'rgb(243,243,243)';
+
+for (let index = 0; index < urgentDivs.length; index += 1) {
+    urgentDivs[index].style.backgroundColor = 'rgb(255,159,132)'
+ }
