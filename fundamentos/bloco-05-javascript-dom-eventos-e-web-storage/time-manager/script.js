2b6b4a658d71d6cd81af580d13d0202536de8557
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-09-28 23:37:02 -0300

    Armazena busca dos h3 de classe 'emergency-tasks' e cria loop para definir cor

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/time-manager/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/time-manager/script.js
@@ -1,10 +1,15 @@
 const header = document.getElementById('header-container');
 const body = document.querySelector('body');
 const urgentDivs = document.getElementsByClassName('emergency-tasks');
+const urgentHeadings = document.querySelectorAll('.emergency-tasks h3');
 
 header.style.backgroundColor = 'rgb(0,176,105)';
 body.style.backgroundColor = 'rgb(243,243,243)';
 
 for (let index = 0; index < urgentDivs.length; index += 1) {
     urgentDivs[index].style.backgroundColor = 'rgb(255,159,132)'
+ }
+
+ for (let index = 0; index < urgentHeadings.length; index += 1) {
+    urgentHeadings[index].style.backgroundColor = 'rgb(165,0,243)'
  }
