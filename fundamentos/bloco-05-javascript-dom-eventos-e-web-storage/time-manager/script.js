08c792ae5f0193d3397188398699a0e89aea93a1
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-09-28 23:43:18 -0300

    Armazena busca das divs de classe no-emergency-tasks e cria loop que define cor

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/time-manager/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/time-manager/script.js
@@ -2,6 +2,7 @@ const header = document.getElementById('header-container');
 const body = document.querySelector('body');
 const urgentDivs = document.getElementsByClassName('emergency-tasks');
 const urgentHeadings = document.querySelectorAll('.emergency-tasks h3');
+const noUrgentDivs = document.getElementsByClassName('no-emergency-tasks');
 
 header.style.backgroundColor = 'rgb(0,176,105)';
 body.style.backgroundColor = 'rgb(243,243,243)';
@@ -12,4 +13,8 @@ for (let index = 0; index < urgentDivs.length; index += 1) {
 
  for (let index = 0; index < urgentHeadings.length; index += 1) {
     urgentHeadings[index].style.backgroundColor = 'rgb(165,0,243)'
+ }
+
+ for (let index = 0; index < noUrgentDivs.length; index += 1) {
+    noUrgentDivs[index].style.backgroundColor = 'rgb(249,219,94)'
  }
