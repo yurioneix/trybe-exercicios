2de526d60b1fd741512ea03df5d799487b8859a1
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-10-02 18:31:02 -0300

    Exercício 2: cria função que retorna o índice do maior valor do array

--- fundamentos/bloco-04-introducao-a-javascript/dia-4-objetos-e-funcoes/funcoes/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-4-objetos-e-funcoes/funcoes/script.js
@@ -1,16 +1,25 @@
 //Exercício 1
-function verificaPalindromo (string) {
-    let reverse = string.split('').reverse().join('');
-    let palindromo = reverse === string ? true : false;
+// function verificaPalindromo (string) {
+//     let reverse = string.split('').reverse().join('');
+//     let palindromo = reverse === string ? true : false;
 
-    return palindromo;
-}
+//     return palindromo;
+// }
 
-console.log(verificaPalindromo('arara'));
-console.log(verificaPalindromo('desenvolvimento'));
+// console.log(verificaPalindromo('arara'));
+// console.log(verificaPalindromo('desenvolvimento'));
 
 // Exercício 2 
 
-function lowerIndexValue (array) {
-    
+function biggerIndex (array) {
+    let biggerIndex = 0;
+
+    for (index in array) {
+        if (array[biggerIndex] < array[index]) {
+            biggerIndex = index;
+        }
+    }
+    console.log(biggerIndex);
 }
+
+biggerIndex([2, 3, 6, 7, 10, 1]);
