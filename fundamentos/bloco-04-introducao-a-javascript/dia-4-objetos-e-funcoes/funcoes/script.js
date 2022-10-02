a3edff51e9f4c6373913ffc80500b4661799347c
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-10-02 17:23:32 -0300

    Transforma string em array de caracteres e reverte seus valores

--- fundamentos/bloco-04-introducao-a-javascript/dia-4-objetos-e-funcoes/funcoes/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-4-objetos-e-funcoes/funcoes/script.js
@@ -1,3 +1,12 @@
 function verificaPalindromo (string) {
-    
+    let arrayString = string.split('')
+    console.log(arrayString);
+    let arrayStringReverse = arrayString.reverse();
+    console.log(arrayStringReverse);
+
+    let palindromo = (arrayString === arrayStringReverse) ? true : false;
+
+    return palindromo;
 }
+
+console.log(verificaPalindromo('beterraba'));
