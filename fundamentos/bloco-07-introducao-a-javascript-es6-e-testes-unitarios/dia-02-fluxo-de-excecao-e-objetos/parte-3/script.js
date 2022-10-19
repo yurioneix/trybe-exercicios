b217e4318f14ea5bd4725cc4ca96035bcd44821b
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-10-19 11:20:30 -0300

    Cria função que retorna o número total de estudantes do objeto allLessons

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-fluxo-de-excecao-e-objetos/parte-3/script.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-fluxo-de-excecao-e-objetos/parte-3/script.js
@@ -31,8 +31,23 @@ const lesson1 = {
 
   const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
 
+  const getNumberOfStudents = (obj) => {
+    let total = 0;
+  
+    const array = Object.keys(obj); // cria um array com lesson1, lesson2, lesson3
+      
+    for (index in array) {
+      total += obj[array[index]].numeroEstudantes; // incremente a variável total a cada iteração
+    }
+    return total; // a função retorna o total de alunos após as iterações do "for/in"
+  };
+
   newProperty(lesson2, 'turno', 'noite');
   listKeys(lesson1);
   objectLength(lesson2);
   listValues(lesson3);
   console.log(allLessons);
+  console.log(getNumberOfStudents(allLessons));
+
+  
+  
