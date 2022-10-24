cc988acf93244c1d829465b6c2f79bc09679911b
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-10-24 18:30:17 -0300

    Cria função que recebe um nome e um sobrenome como parâmetros e retorna um objeto contendo um email com esse nome

---
+++ fundamentos/bloco-08-high-order-functions/exercicio1.js
@@ -0,0 +1,15 @@
+const generateEmail = (name, lastname) => {
+    const obj = {
+        nomeCompleto: name + lastname,
+        email: `${name}_${lastname}@trybe.com`,
+    }
+    return obj;
+}
+const newEmployees = () => {
+    const employees = {
+      id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+  };
