8f393583e454b13caa824cf02934c48b614c3b5f
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-10-24 18:42:50 -0300

    Resolve exercício 1 (olhando gabarito)

--- fundamentos/bloco-08-high-order-functions/exercicio1.js
+++ fundamentos/bloco-08-high-order-functions/exercicio1.js
@@ -1,15 +1,14 @@
-const generateEmail = (name, lastname) => {
-    const obj = {
-        nomeCompleto: name + lastname,
-        email: `${name}_${lastname}@trybe.com`,
-    }
-    return obj;
+const generateEmail = (fullName) => {
+    const email = fullName.toLowerCase().split(' ').join('_');
+    return { nomeCompleto:`${fullName}`, email: `${email}@trybe.com` };
 }
-const newEmployees = () => {
+const newEmployees = (callback) => {
     const employees = {
-      id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+      id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
     }
     return employees;
   };
+
+  console.log(newEmployees(generateEmail));
