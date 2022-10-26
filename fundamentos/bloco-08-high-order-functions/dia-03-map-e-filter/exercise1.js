f98fe596b51d142775323950af138bba3ed2af20
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-10-26 19:58:15 -0300

    Cria estrutura do requisito 4

--- fundamentos/bloco-08-high-order-functions/dia-03-map-e-filter/exercise1.js
+++ fundamentos/bloco-08-high-order-functions/dia-03-map-e-filter/exercise1.js
@@ -82,3 +82,31 @@ const books = [
   }
 
   console.log(fantasyOrScienceFiction());
+
+  const expectedResult = [
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+      releaseYear: 1928,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: { name: 'Isaac Asimov', birthYear: 1920 },
+      releaseYear: 1951,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+      releaseYear: 1954,
+    },
+  ];
+  
+  function oldBooksOrdered() {
+    // escreva seu código aqui
+  }
