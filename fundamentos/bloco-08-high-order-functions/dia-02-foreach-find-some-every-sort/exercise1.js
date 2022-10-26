888aa168886b1b50e4545c6b3f04b20257d72fdd
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-10-25 23:20:42 -0300

    Encontra o primeiro autor que nasceu em 1947

--- fundamentos/bloco-08-high-order-functions/dia-02-foreach-find-some-every-sort/exercise1.js
+++ fundamentos/bloco-08-high-order-functions/dia-02-foreach-find-some-every-sort/exercise1.js
@@ -62,6 +62,6 @@ const books = [
   ];
   
   // Adicione o código do exercício aqui:
-  function authorBornIn1947() {
-    // escreva aqui o seu código
-  }
+  const authorBornIn1947 = () => books.find((book) => book.author.birthYear === 1947).author.name;
+
+  console.log(authorBornIn1947(books));
