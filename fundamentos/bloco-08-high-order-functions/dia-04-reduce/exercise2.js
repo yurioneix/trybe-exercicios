79e70bc293a9a54b08f2b3ecf13a3aa644eae1b5
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-10-27 19:24:13 -0300

    Requisito 3 - exercício 2: encontre o livro com maior nome

--- fundamentos/bloco-08-high-order-functions/dia-04-reduce/exercise2.js
+++ fundamentos/bloco-08-high-order-functions/dia-04-reduce/exercise2.js
@@ -73,3 +73,19 @@ const books = [
     .reduce((acc, curr) => Math.round(acc + curr / array.length), 0);
 
   console.log(averageAge(books))
+
+
+const expectedResult = {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  };
+  
+  const longestNamedBook = (array) => array.reduce((acc, curr) => acc);
+
+  console.log(longestNamedBook(books));
